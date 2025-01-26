f6c88ecea668654bef3bc2aced97e5db8b14b76f
Sneh Patel <patel.sneh2102@gmail.com>
2025-01-26 07:59:24 -0400

    Added build files

---
+++ build/Release/.deps/Release/obj.target/liboqs_node/src/Signature.o.d
@@ -0,0 +1,75 @@
+cmd_Release/obj.target/liboqs_node/src/Signature.o := g++ -o Release/obj.target/liboqs_node/src/Signature.o ../src/Signature.cpp '-DNODE_GYP_MODULE_NAME=liboqs_node' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DNAPI_CPP_EXCEPTIONS' '-DNAPI_VERSION=6' '-DNODE_API_NO_EXTERNAL_BUFFERS_ALLOWED' '-DBUILDING_NODE_EXTENSION' -I/home/sneh/.cache/node-gyp/22.13.0/include/node -I/home/sneh/.cache/node-gyp/22.13.0/src -I/home/sneh/.cache/node-gyp/22.13.0/deps/openssl/config -I/home/sneh/.cache/node-gyp/22.13.0/deps/openssl/openssl/include -I/home/sneh/.cache/node-gyp/22.13.0/deps/uv/include -I/home/sneh/.cache/node-gyp/22.13.0/deps/zlib -I/home/sneh/.cache/node-gyp/22.13.0/deps/v8/include -I/home/sneh/Desktop/TeleAI/liboqs/liboqs-node/node_modules/node-addon-api -I../deps/liboqs/build/include -I../deps/liboqs-cpp/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -fexceptions -std=c++2a -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -fno-strict-aliasing -std=gnu++17 -fexceptions -std=c++2a -MMD -MF ./Release/.deps/Release/obj.target/liboqs_node/src/Signature.o.d.raw   -c
+Release/obj.target/liboqs_node/src/Signature.o: ../src/Signature.cpp \
+ ../src/Signature.h \
+ /home/sneh/Desktop/TeleAI/liboqs/liboqs-node/node_modules/node-addon-api/napi.h \
+ /home/sneh/.cache/node-gyp/22.13.0/include/node/node_api.h \
+ /home/sneh/.cache/node-gyp/22.13.0/include/node/js_native_api.h \
+ /home/sneh/.cache/node-gyp/22.13.0/include/node/js_native_api_types.h \
+ /home/sneh/.cache/node-gyp/22.13.0/include/node/node_api_types.h \
+ /home/sneh/Desktop/TeleAI/liboqs/liboqs-node/node_modules/node-addon-api/napi-inl.h \
+ /home/sneh/Desktop/TeleAI/liboqs/liboqs-node/node_modules/node-addon-api/napi.h \
+ /home/sneh/Desktop/TeleAI/liboqs/liboqs-node/node_modules/node-addon-api/napi-inl.deprecated.h \
+ ../deps/liboqs-cpp/include/oqs_cpp.hpp \
+ ../deps/liboqs-cpp/include/common.hpp \
+ ../deps/liboqs/build/include/oqs/common.h \
+ ../deps/liboqs/build/include/oqs/oqsconfig.h \
+ ../deps/liboqs/build/include/oqs/oqs.h \
+ ../deps/liboqs/build/include/oqs/rand.h \
+ ../deps/liboqs/build/include/oqs/kem.h \
+ ../deps/liboqs/build/include/oqs/kem_bike.h \
+ ../deps/liboqs/build/include/oqs/kem_classic_mceliece.h \
+ ../deps/liboqs/build/include/oqs/kem_hqc.h \
+ ../deps/liboqs/build/include/oqs/kem_kyber.h \
+ ../deps/liboqs/build/include/oqs/kem_ml_kem.h \
+ ../deps/liboqs/build/include/oqs/kem_ntruprime.h \
+ ../deps/liboqs/build/include/oqs/kem_frodokem.h \
+ ../deps/liboqs/build/include/oqs/sig.h \
+ ../deps/liboqs/build/include/oqs/sig_dilithium.h \
+ ../deps/liboqs/build/include/oqs/sig_ml_dsa.h \
+ ../deps/liboqs/build/include/oqs/sig_falcon.h \
+ ../deps/liboqs/build/include/oqs/sig_sphincs.h \
+ ../deps/liboqs/build/include/oqs/sig_mayo.h \
+ ../deps/liboqs/build/include/oqs/sig_cross.h \
+ ../deps/liboqs/build/include/oqs/sig_stfl.h \
+ ../deps/liboqs/build/include/oqs/aes_ops.h \
+ ../deps/liboqs/build/include/oqs/sha2_ops.h \
+ ../deps/liboqs/build/include/oqs/sha3_ops.h \
+ ../deps/liboqs/build/include/oqs/sha3x4_ops.h \
+ ../deps/liboqs-cpp/include/common.hpp
+../src/Signature.cpp:
+../src/Signature.h:
+/home/sneh/Desktop/TeleAI/liboqs/liboqs-node/node_modules/node-addon-api/napi.h:
+/home/sneh/.cache/node-gyp/22.13.0/include/node/node_api.h:
+/home/sneh/.cache/node-gyp/22.13.0/include/node/js_native_api.h:
+/home/sneh/.cache/node-gyp/22.13.0/include/node/js_native_api_types.h:
+/home/sneh/.cache/node-gyp/22.13.0/include/node/node_api_types.h:
+/home/sneh/Desktop/TeleAI/liboqs/liboqs-node/node_modules/node-addon-api/napi-inl.h:
+/home/sneh/Desktop/TeleAI/liboqs/liboqs-node/node_modules/node-addon-api/napi.h:
+/home/sneh/Desktop/TeleAI/liboqs/liboqs-node/node_modules/node-addon-api/napi-inl.deprecated.h:
+../deps/liboqs-cpp/include/oqs_cpp.hpp:
+../deps/liboqs-cpp/include/common.hpp:
+../deps/liboqs/build/include/oqs/common.h:
+../deps/liboqs/build/include/oqs/oqsconfig.h:
+../deps/liboqs/build/include/oqs/oqs.h:
+../deps/liboqs/build/include/oqs/rand.h:
+../deps/liboqs/build/include/oqs/kem.h:
+../deps/liboqs/build/include/oqs/kem_bike.h:
+../deps/liboqs/build/include/oqs/kem_classic_mceliece.h:
+../deps/liboqs/build/include/oqs/kem_hqc.h:
+../deps/liboqs/build/include/oqs/kem_kyber.h:
+../deps/liboqs/build/include/oqs/kem_ml_kem.h:
+../deps/liboqs/build/include/oqs/kem_ntruprime.h:
+../deps/liboqs/build/include/oqs/kem_frodokem.h:
+../deps/liboqs/build/include/oqs/sig.h:
+../deps/liboqs/build/include/oqs/sig_dilithium.h:
+../deps/liboqs/build/include/oqs/sig_ml_dsa.h:
+../deps/liboqs/build/include/oqs/sig_falcon.h:
+../deps/liboqs/build/include/oqs/sig_sphincs.h:
+../deps/liboqs/build/include/oqs/sig_mayo.h:
+../deps/liboqs/build/include/oqs/sig_cross.h:
+../deps/liboqs/build/include/oqs/sig_stfl.h:
+../deps/liboqs/build/include/oqs/aes_ops.h:
+../deps/liboqs/build/include/oqs/sha2_ops.h:
+../deps/liboqs/build/include/oqs/sha3_ops.h:
+../deps/liboqs/build/include/oqs/sha3x4_ops.h:
+../deps/liboqs-cpp/include/common.hpp:
---
+++ build/Release/.deps/Release/oqs_node.node.d
@@ -0,0 +1,1 @@
+cmd_Release/oqs_node.node := ln -f "Release/obj.target/oqs_node.node" "Release/oqs_node.node" 2>/dev/null || (rm -rf "Release/oqs_node.node" && cp -af "Release/obj.target/oqs_node.node" "Release/oqs_node.node")
